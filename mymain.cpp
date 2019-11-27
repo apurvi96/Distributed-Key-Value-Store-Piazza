6f0555a72d87ce8cf073c3870b7c4a0d68115211
arushi <arushi.233@gmail.com>
2019-11-27 17:28:28 +0530

    update jsonstring objects

--- mymain.cpp
+++ mymain.cpp
@@ -16,6 +16,7 @@ int main(){
 	// In-situ parsing, decode strings directly in the source string. Source must be string. And paramater must not be const char*
 	doc.ParseInsitu((char*)s.c_str());
 	cout<<"role= "<<doc["role"].GetString()<<endl;
+	
 
 
 }
